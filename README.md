7df3cf65c7b199d9fd5a6ef55ec66237c1828b6a
goodgoodman <gjwnsan9420@gmail.com>
2021-11-16 22:54:28 +0900

    Initialize repository

---
+++ README.md
@@ -0,0 +1,3 @@
+# Svelte with TS, Tailwind
+
+Svelte와 TS, Tailwind 설정이 된 템플릿입니다.
---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<style lang="postcss" global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<script lang="ts">
+  export let name: string
+</script>
+
+<main>
+  <h1 class="flex flex-row-reverse text-blue-300">Hello {name}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build
+    Svelte apps.asdasaasdas
+  </p>
+</main>
